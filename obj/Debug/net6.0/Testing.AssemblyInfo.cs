38ec226fbe7306c4240d6182906497660d223813
Justin Simon <justinsimon@Justins-MBP.lan>
2024-09-07 15:01:52 -0700

    Created functioning auth

--- obj/Debug/net6.0/Testing.AssemblyInfo.cs
+++ obj/Debug/net6.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931f70650abd21a2a24251c1081bc12fc4270cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9442f5e99bd4a7a9df21ec8a1f29c4c21ec1414e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
